e710499bfba4ceb2f2fbb2a43772486bf46943a4
Mauro Risonho de Paula Assumpção <mauro.risonho@gmail.com>
2020-10-13 22:17:38 -0300

    add Values Integer/Float/Booleans/Logical operators

--- 02Values/Values.go
+++ 02Values/Values.go
@@ -1,8 +1,22 @@
+// Go tem vários tipos de valor,
+// incluindo strings, inteiros, flutuantes, booleanos, etc.
+// Aqui estão alguns exemplos básicos.
+
 package main
 
 import "fmt"
 
 func main() {
+
+	//Strings, que podem ser adicionados junto com +
 	fmt.Println("go" + "lang")
 
+	//Inteiros e flutuantes.
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	//Booleanos, com operadores booleanos como seria de esperar.
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
 }
